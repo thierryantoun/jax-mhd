6746a4cf576bcdd1235ca64f0c16170328533d28
thierry antoun <thierry.antoun@cea.fr>
2025-08-26 14:59:17 +0200

    first order hydro

--- clean_workflow_opt_firstorder/physics.py
+++ clean_workflow_opt_firstorder/physics.py
@@ -1,20 +1,20 @@
 from modules import *
 
 @jax.jit
-def get_conserved(rho, vx, vy, vz, P, gamma, Bx, By, Bz):
+def get_conserved(rho, vx, vy, vz, P, gamma):
     Mass = rho
     Momx = rho * vx
     Momy = rho * vy
     Momz = rho * vz
-    Energy = (P / (gamma - 1) + 0.5 * rho * (vx**2 + vy**2 + vz**2) + 0.5 * (Bx**2 + By**2 + Bz**2))
-    return Mass, Momx, Momy, Momz, Energy, Bx, By, Bz
+    Energy = (P / (gamma - 1) + 0.5 * rho * (vx**2 + vy**2 + vz**2))
+    return Mass, Momx, Momy, Momz, Energy
 
 
 @jax.jit
-def get_primitive(Mass, Momx, Momy, Momz, Energy, gamma, Bx, By, Bz):
+def get_primitive(Mass, Momx, Momy, Momz, Energy, gamma):
     rho = Mass
     vx = Momx / rho
     vy = Momy / rho
     vz = Momz / rho
-    P = (Energy - 0.5 * rho * (vx**2 + vy**2 + vz**2) - 0.5 * (Bx**2 + By**2 + Bz**2)) * (gamma - 1)
-    return rho, vx, vy, vz, P, Bx, By, Bz
+    P = (Energy - 0.5 * rho * (vx**2 + vy**2 + vz**2)) * (gamma - 1)
+    return rho, vx, vy, vz, P
