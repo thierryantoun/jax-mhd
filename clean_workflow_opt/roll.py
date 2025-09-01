24a9c1816cec88bdd761233886eb5b6d0f406fc8
thierry antoun <thierry.antoun@cea.fr>
2025-09-01 15:53:12 +0200

    test roll perf

--- clean_workflow_opt/roll.py
+++ clean_workflow_opt/roll.py
@@ -2,13 +2,13 @@ import jax.lax as lax
 import jax.numpy as jnp
 
 def shift_left(f, axis):
-    n = f.shape[axis]
-    idx = (jnp.arange(n) - 1) % n     
-    return jnp.take(f, idx, axis=axis)
+    # n = f.shape[axis]
+    # idx = (jnp.arange(n) - 1) % n     
+    return jnp.roll(f, 1, axis= axis)
 
 def shift_right(f, axis):
-    n = f.shape[axis]
-    idx = (jnp.arange(n) + 1) % n     
-    return jnp.take(f, idx, axis=axis)
+    # n = f.shape[axis]
+    # idx = (jnp.arange(n) + 1) % n     
+    return jnp.roll(f, 1, axis= axis)
 
 
