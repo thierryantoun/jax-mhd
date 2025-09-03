c8a3836e3d760a671ab72ffe18aa584f146aaf36
thierry antoun <thierry.antoun@cea.fr>
2025-09-03 10:22:28 +0200

    optim

--- clean_workflow_opt/numerical_scheme.py
+++ clean_workflow_opt/numerical_scheme.py
@@ -1,7 +1,7 @@
 import jax
 import jax.numpy as jnp
 from physics import get_primitive
-from jaxpr import examine_jaxpr
+# from jaxpr import examine_jaxpr
 
 @jax.jit
 def update(rho, Momx, Momy, Momz, Energy, dx, dy, dz, gamma, courant_fac, Bx, By, Bz):
