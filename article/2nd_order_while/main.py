f8f35b910c0e45ccdcd97c841e9d9953589b15c4
thierry antoun <thierry.antoun@cea.fr>
2026-03-05 10:23:24 +0100

    while

--- article/2nd_order_while/main.py
+++ article/2nd_order_while/main.py
@@ -75,31 +75,28 @@ def main(args, config):
     # -------------------------
     # PROFILING
     # -------------------------
+    # jax.profiler.start_trace("/tmp/jax-trace")
+
     global_start = time.time()
+
     t = 0.0
     n_iter = 0
-    trace_done = False
 
     while t < t_stop:
-        
-        # Démarrer la trace après quelques itérations de warmup
-        if n_iter == 5 and not trace_done:
-            jax.profiler.start_trace("/tmp/while-trace")
 
         with jax.profiler.TraceAnnotation("update"):
             Mass, Momx, Momy, Momz, Energy, dt, Bx, By, Bz = update(
                 Mass, Momx, Momy, Momz, Energy, dx, dy, dz, gamma, courant_fac, Bx, By, Bz
             )
+
         t += dt
         n_iter += 1
+
         jax.block_until_ready((Mass, Momx, Momy, Momz, Energy, Bx, By, Bz))
 
-        # Arrêter après 10 itérations tracées
-        if n_iter == 15 and not trace_done:
-            jax.profiler.stop_trace()
-            trace_done = True
-
     global_end = time.time()
+
+    # jax.profiler.stop_trace()
 
     # KPIs
     total_time = global_end - global_start
